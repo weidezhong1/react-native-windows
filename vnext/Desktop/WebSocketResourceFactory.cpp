f5f010ad652e07eaca788022ca5af84c230ffc4e
Julio C. Rocha <julio.rocha@microsoft.com>
2020-05-20 22:33:22 -0700

    First batch of files

--- vnext/Desktop/WebSocketResourceFactory.cpp
+++ vnext/Desktop/WebSocketResourceFactory.cpp
@@ -3,7 +3,6 @@
 
 #include "pch.h"
 
-#include <WinRTWebSocketResource.h>
 #include "BeastWebSocketResource.h"
 
 using std::make_shared;
@@ -16,31 +15,20 @@ namespace Microsoft::React {
 /*static*/
 shared_ptr<IWebSocketResource>
 IWebSocketResource::Make(const string &urlString, bool legacyImplementation, bool acceptSelfSigned) {
-  if (!legacyImplementation) {
-    std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult> certExceptions;
-    if (acceptSelfSigned) {
-      certExceptions.emplace_back(
-          winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult::Untrusted);
-      certExceptions.emplace_back(
-          winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult::InvalidName);
-    }
-    return make_shared<WinRTWebSocketResource>(urlString, certExceptions);
+  Url url(urlString);
+
+  if (url.scheme == "ws") {
+    if (url.port.empty())
+      url.port = "80";
+
+    return make_shared<Beast::WebSocketResource>(std::move(url));
+  } else if (url.scheme == "wss") {
+    if (url.port.empty())
+      url.port = "443";
+
+    return make_shared<Beast::SecureWebSocket>(std::move(url));
   } else {
-    Url url(urlString);
-
-    if (url.scheme == "ws") {
-      if (url.port.empty())
-        url.port = "80";
-
-      return make_shared<Beast::WebSocketResource>(std::move(url));
-    } else if (url.scheme == "wss") {
-      if (url.port.empty())
-        url.port = "443";
-
-      return make_shared<Beast::SecureWebSocket>(std::move(url));
-    } else {
-      throw std::invalid_argument((string("Incorrect URL scheme: ") + url.scheme).c_str());
-    }
+    throw std::invalid_argument((string("Incorrect URL scheme: ") + url.scheme).c_str());
   }
 }
 
